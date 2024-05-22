34c4c7bb745f420481c1891d258c54953b3d9fc0
Lucas Gabriel Pestana <lucasgabrielpestana03@gmail.com>
2024-05-22 13:24:45 -0300

    mod: Modify indexes of rows and columns from str to float

--- data_processing.py
+++ data_processing.py
@@ -4,6 +4,17 @@ import tabula
 import os
 import sys
 
+# Trata o formato dos valores str dos índices das colunas e linhas e os converte para float
+def convert_axes_to_float(df: pd.DataFrame) -> pd.DataFrame:
+
+  df.columns = df.columns.str.replace(",", ".")
+  df.columns = df.columns.astype(float)
+  
+  df.index = df.index.str.replace(",", ".")
+  df.index = df.index.astype(float)
+
+  return df
+
 def get_database(filepath: str):
 
   if not os.path.isfile(filepath):
@@ -18,47 +29,8 @@ def get_database(filepath: str):
 
   return tabula.read_pdf(filepath, encoding="latin-1")
 
-df_z_values_1 = get_database("z-table-part-1.pdf")[0]
-df_z_values_2 = get_database("z-table-part-2.pdf")[0]
-
-if sys.platform == "win32":
-  os.system("cls")
-else:
-  os.system("clear")
-
-# Removendo coluna (e Series) com valores nulos
-df_z_values_1 = df_z_values_1.drop("0", axis=1)
-
-# Alterando os labels das Series de coluna para os valores correspondentes a 2a casa decimal de z
-df_z_values_1.columns = df_z_values_1.loc[0].values
-
-# Alterando os índices do DataFrama para os valores correspondentes ao inteiro e 1a casa decimal de z
-df_z_values_1 = df_z_values_1.set_index(df_z_values_1['z'].values)
-
-# Removendo a Series linha e Series coluna de label z, usada anteriormente para definir os índices
-df_z_values_1 = df_z_values_1.drop('z', axis=1)
-df_z_values_1 = df_z_values_1.drop('z', axis=0)
-
-# Pegando a parte dos valores negativos na tabela, e adicionando o sinal de negativo neles
-negative_indexes = list(map(lambda x: str(-float(x.replace(",", "."))).replace(".", ","), df_z_values_1.index[:-5]))
-
-# Pegando a parte dos valores positivos
-positive_indexes = list(df_z_values_1.index[-5:])
-
-# Alterando os índices do DataFrame para os valores obtidos anteriormente
-df_z_values_1.index = negative_indexes + positive_indexes
-
-# Modificando os valores dos indíces a partir de uma series que representa um coluna
-df_z_values_2 = df_z_values_2.set_index(df_z_values_2['z'].values)
-
-# Removendo a coluna (e Series) com o name 'z'
-df_z_values_2 = df_z_values_2.drop('z', axis=1)
-
-# Concatena as duas partes da tabela, obtidas no livro do Devore
-Z_VALUES = pd.concat((df_z_values_1, df_z_values_2))
-
 # Pega a porcentagem correspondente ao z percentil passado como argumento
-def get_percentage_value(z_percentile: str, df: pd.DataFrame=Z_VALUES):
+def get_percentage_value(z_percentile: str, df: pd.DataFrame):
 
   return df.loc[*process_input(z_percentile)]
 
@@ -70,15 +42,55 @@ def process_input(input_value):
   # Adiciona 2 casas decimais no número, caso não tenha, e retorna a ser str
   input_value = "{:.2f}".format(input_value)
 
-  # Volta a ter vírgula em vez do ponto
-  input_value = input_value.replace(".", ",")
-
   # Decompoe o número em duas partes: a primeira sendo o inteiro e a primeira casa decimal e o segundo a segunda casa decimal
-  # Caso o número seja negativo, a quantidade de caracteres pegos para a primeira parte acrescenta 1 (referente ao sinal de negativo)
-  if input_value.startswith("-"):
+  return (float(input_value[:-1]), float("0.0" + input_value[-1]))
 
-    return (input_value[:4], "0,0" + input_value[-1])
-  
-  else:
+filepaths = list(map(lambda x: os.path.join("files", x), os.listdir("files")))
 
-    return (input_value[:3], "0,0" + input_value[-1])
+df_z_values_1 = get_database(filepaths[0])[0]
+df_z_values_2 = get_database(filepaths[1])[0]
+
+if sys.platform == "win32":
+  os.system("cls")
+else:
+  os.system("clear")
+
+# Removendo a coluna (e a Series que a reprensta) com valores nulos
+df_z_values_1 = df_z_values_1.drop("0", axis=1)
+
+# Definindo o nome das colunas e guardando-as em uma variável auxiliar
+column_values = df_z_values_1.loc[0].values[1:]
+
+df_z_values_1 = df_z_values_1.set_index(df_z_values_1["Unnamed: 0"].values)
+
+df_z_values_1 = df_z_values_1.drop("Unnamed: 0", axis=1)
+df_z_values_1 = df_z_values_1.drop('z', axis=0)
+
+# Alterando os nomes das colunas para os valores correspondentes a 2a casa decimal de z
+df_z_values_1.columns = column_values
+
+df_z_values_1 = convert_axes_to_float(df_z_values_1)
+
+# Gerando índices negativos que não foram lidos do pdf
+negative_indexes = list(map(lambda x: -x, df_z_values_1.index[:-5]))
+
+# Pegando a parte dos valores positivos
+positive_indexes = list(df_z_values_1.index[-5:])
+
+# Alterando os índices do DataFrame para os valores correspondentes ao inteiro e 1a casa decimal de z
+df_z_values_1.index = negative_indexes + positive_indexes
+
+# Modificando os valores dos indíces a partir de uma series que representa um coluna
+df_z_values_2 = df_z_values_2.set_index(df_z_values_2['z'].values)
+
+# Removendo a coluna (e Series) com o name 'z'
+df_z_values_2 = df_z_values_2.drop('z', axis=1)
+
+df_z_values_2 = convert_axes_to_float(df_z_values_2)
+
+# Concatena as duas partes da tabela, obtidas no livro do Devore
+Z_VALUES = pd.concat((df_z_values_1, df_z_values_2))
+
+Z_VALUES = Z_VALUES.apply(lambda x: x.str.replace(",", ".").astype(float))
+
+print(str(get_percentage_value("2,71", Z_VALUES)).replace(".", ","))
---
+++ read_data.py
@@ -0,0 +1,5 @@
+from data_processing import get_percentage_value
+
+Z_VALUE = "-2,47"
+
+print(f"A porcentagem correspondente ao z percentil {Z_VALUE} é {get_percentage_value(Z_VALUE)}")
