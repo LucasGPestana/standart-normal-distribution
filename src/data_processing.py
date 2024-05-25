c5823405bbd2342006527141a47ebe5fa3f12ebe
Lucas Gabriel Pestana <lucasgabrielpestana03@gmail.com>
2024-05-25 12:44:57 -0300

    mod: Modifying functions and local variable names and adding some comments

--- src/data_processing.py
+++ src/data_processing.py
@@ -4,7 +4,7 @@ import tabula
 import os
 import sys
 
-from typing import Tuple, List
+from typing import List
 
 # Trata o formato dos valores str dos índices das colunas e linhas e os converte para float
 def convert_axes_to_float(df: pd.DataFrame) -> pd.DataFrame:
@@ -34,7 +34,7 @@ def get_database(filepath: str):
 # Pega a porcentagem correspondente ao z percentil passado como argumento
 def get_percentage_value(z_percentile: str, df: pd.DataFrame):
 
-  percentage = df.loc[*process_input(z_percentile)]
+  percentage = df.loc[*process_z_input(z_percentile)]
 
   if isinstance(percentage, pd.Series):
 
@@ -42,11 +42,12 @@ def get_percentage_value(z_percentile: str, df: pd.DataFrame):
   
   return percentage
 
-# Verifica se existe a porcentagem exata e, caso não, retorna uma lista contendo
+# Verifica se a porcentagem passada existe, de forma exata, no DataFrame
+# Caso não, são pegos o maior valor menor que a porcentagem e o menor valor maior que a porcentagem no DataFrame
 def verify_percentage_situations(percentage: str, 
                                  df: pd.DataFrame, 
                                  row_index: float, 
-                                 column_pos_index: int) -> List | float:
+                                 column_pos_index: int) -> List:
   
   iter_percentage_value = df.loc[row_index][df.columns[column_pos_index]]
 
@@ -56,10 +57,12 @@ def verify_percentage_situations(percentage: str,
 
   )
 
+  # A verificação do sinal do "row_index" garante que a operação aritmética de soma, para obter o valor z, seja feita corretamente
   if iter_percentage_value == percentage:
 
     return [row_index, df.columns[column_pos_index]] if row_index > 0 else [row_index, -df.columns[column_pos_index]]
   
+  # A mesma verificação é feita aqui
   if iter_percentage_value > percentage:
 
     return (
@@ -67,48 +70,54 @@ def verify_percentage_situations(percentage: str,
       else [row_index, -df.columns[column_pos_index + 1], -df.columns[column_pos_index]]
     )
 
+# Pega o valor z correspondente a porcentagem passada como argumento
 def get_z_value(percentage: str, df: pd.DataFrame) -> float:
 
-  possible_values = list() # Caso não tenha o valor exato da porcentagem
+  possible_values_parts = list() # Representa as partes que constituem o valor z, ou seja, o indice da linha e os possíveis indices de coluna
   percentage = process_percentage_value(percentage)
 
+  # A tabela z segue um padrão no qual, caso o índice da linha seja positivo, o indice da coluna é diretamente proporcional ao valor da porcentagem. No caso de indices de linha negativos, essas grandezas são inversas
   for index in df.index:
 
     if index < 0:
 
+      # Devido a característica de proporcionalidade e a lógica de pegar os índices de coluna a partir da próxima porcentagem da tabela que passar de "percentage", é necessário iterar o objeto range dos indices posicionais das colunas de trás para frente
       for pos_column in range(len(df.columns) - 1, -1, -1):
 
-        possible_values = verify_percentage_situations(percentage, df, index, pos_column)
+        possible_values_parts = verify_percentage_situations(percentage, df, index, pos_column)
 
-        if possible_values: break
+        if possible_values_parts: break
       
-      if possible_values: break
+      if possible_values_parts: break
     
     else:
 
+      # Como aqui o indice da coluna e a porcentagem são diretamente proporcionais, a iteração é feita de frente para trás
       for pos_column in range(0 ,len(df.columns), 1):
 
-        possible_values = verify_percentage_situations(percentage, df, index, pos_column)
+        possible_values_parts = verify_percentage_situations(percentage, df, index, pos_column)
 
-        # print(f"Linha: {index}\nColuna: {df.columns[pos_column]}\nValores possíveis: {possible_values}\n")
-
-        if possible_values: break
+        if possible_values_parts: break # Evita que novas iterações no loop interno sejam feitas depois que os valores são achados
       
-      if possible_values: break
+      if possible_values_parts: break # Mesmo objetivo do de cima, só que para o loop externo
   
+  # São pegos as distâncias entre os valores de porcentagem correspondentes àquele valor z com relação a "percentage"
   # O primeiro valor não é considerado pois se refere ao indice da linha selecionada
-  distances = list(map(lambda x: abs(df.loc[possible_values[0]][abs(x)] - percentage), possible_values[1:]))
+  # A troca de sinais feita na função verify_percentage_situations para o índice das colunas será "corrigida" com a função abs (módulo)
+  distances = list(map(lambda x: abs(df.loc[possible_values_parts[0]][abs(x)] - percentage), possible_values_parts[1:]))
   minor_dist = 100000 # Representa a menor distância entre os valores
 
+  # Pega o índice da coluna que possui a menor distância de porcentagem em relação a "percentage"
   for i in range(len(distances)):
 
     if minor_dist > distances[i]:
 
       minor_dist = distances[i]
-      more_close_value = possible_values[1:][i]
+      more_close_value = possible_values_parts[1:][i]
   
-  return possible_values[0] + more_close_value
+  return possible_values_parts[0] + more_close_value
 
+# Trata a entrada de porcentagem do usuário via linha de comando
 def process_percentage_value(percentage: str) -> float:
 
   percentage = float(percentage.replace(',', '.'))
@@ -117,7 +126,8 @@ def process_percentage_value(percentage: str) -> float:
 
   return percentage
 
-def process_input(input_value: str) -> float:
+# Trata a entrada de valor z do usuário via linha de comando
+def process_z_input(input_value: str) -> float:
 
   # Altera a vírgula no número (caso tenha) para um ponto, e converte para float
   input_value = float(input_value.replace(",", "."))
@@ -128,6 +138,7 @@ def process_input(input_value: str) -> float:
   # Decompoe o número em duas partes: a primeira sendo o inteiro e a primeira casa decimal e o segundo a segunda casa decimal
   return (float(input_value[:-1]), float("0.0" + input_value[-1]))
 
+os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 filepaths = list(map(lambda x: os.path.join("files", x), os.listdir("files")))
 
 df_z_values_1 = get_database(filepaths[0])[0]
